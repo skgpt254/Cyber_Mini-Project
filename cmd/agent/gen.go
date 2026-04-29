b5670a10eaebd2e4a7461075513190270be8c855
Sandesh <guptask0722@gmail.com>
2026-04-29 16:16:03 +0530

    Remove old project structure

--- cmd/agent/gen.go
+++
@@ -1,4 +0,0 @@
-package main
-
-// This command compiles the C code when you run 'go generate'
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -output-dir . monitor ../../bpf/monitor.bpf.c
